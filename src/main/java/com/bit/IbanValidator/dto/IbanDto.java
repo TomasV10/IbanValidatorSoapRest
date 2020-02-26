d5922bf1d05f1d567d707786b284bb981125e6fc
TomasV10 <tomasvarnauskas19@gmail.com>
2020-02-26 14:11:58 +0200

    deleted Strin iban field

--- src/main/java/com/bit/IbanValidator/dto/IbanDto.java
+++ src/main/java/com/bit/IbanValidator/dto/IbanDto.java
@@ -4,17 +4,7 @@ import com.bit.IbanValidator.validation.IbanValidated;
 import java.util.List;
 
 public class IbanDto {
-    private String iban;
     private List<IbanValidated> validatedIbans;
-
-
-    public String getIban() {
-        return iban;
-    }
-
-    public void setIban(String iban) {
-        this.iban = iban;
-    }
 
     public List<IbanValidated> getValidatedIbans() {
         return validatedIbans;
